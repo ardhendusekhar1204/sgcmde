7cc7fe835f8250fe90f86b02d7948e0c1ca89581
Ardhendu Sekhar <ardhendusekhar@protonmail.com>
2025-11-12 14:51:58 +0530

    Remove debug print statement from summary_folds
    Remove print statement for method in summary_folds.

--- train.py
+++ train.py
@@ -49,7 +49,6 @@ def detach(data):
     return detached_data
     
 def summary_folds(root_dir,target='concordance_time_dependent',method='max'):
-    print('method',method)
     vals_dict = {}
     res = {}
     if method=='last':
